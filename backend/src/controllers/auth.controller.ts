a5bfd8c527c7c5614521ead4b881d62a74c41e3a
javad yousefi <javadyousefi9000@gmail.com>
2024-12-18 16:44:43 +0330

    add user data at login and register

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -63,6 +63,7 @@ export const login = async (req: Request, res: Response, next: NextFunction): Pr
         res.status(200).json({
             success: true,
             message: 'Login successful.',
+            data: { name: user.name, email: user.email }
         });
     } catch (error) {
         next(error); // Pass errors to the error handler middleware
