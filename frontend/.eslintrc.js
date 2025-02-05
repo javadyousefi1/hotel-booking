52ee94eeb9cbed839af7421f47518bf55db553fd
javadyousefi <javadyousefi9000@gmail.com>
2025-02-04 22:47:48 -0800

    add eslint from first

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    extends: ["next/core-web-vitals", "eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Next.js doesn't require React import
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // Warn for unused vars except with _
+      "unused-imports/no-unused-imports": "error", // Auto-remove unused imports
+      "import/order": [
+        "warn",
+        {
+          "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "alphabetize": { "order": "asc", "caseInsensitive": true },
+        }
+      ],
+    },
+  };
+  
